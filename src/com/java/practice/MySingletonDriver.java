7bc21abfef3cd9ce427fa0f18a2ba550ac5f3da4
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-06-16 11:29:04 +0530

    Added different ways for Singleton intialization

--- src/com/java/practice/MySingletonDriver.java
+++ src/com/java/practice/MySingletonDriver.java
@@ -11,45 +11,89 @@ A static factory method for obtaining the instance
 
 public class MySingletonDriver {
 
-    public static void main(String[] args) {
-        MySingleton ms1 = MySingleton.getInstance();
-        MySingleton ms2 = MySingleton.getInstance();
-        MySingleton ms3 = MySingleton.getInstance();
+	public static void main(String[] args) {
 
-        System.out.println(ms1.getInfo());
-        System.out.println("************");
-
-        ms2.setInfo("I'm a modified info");
-
-        System.out.println(ms1.getInfo());
-        System.out.println(ms2.getInfo());
-
-    }
+	}
 
 }
 
+/*
+ * We can define the Singleton class in such a way that only one instance will
+ * be get created.
+ * 
+ * 1 . A private constructor so that no one can create the instance explicitly 2
+ * . A private instance variable which get returned from the getter. 3 . A
+ * public getter method to return the instance. If not created then create the
+ * instance otherwise returned the already created one.
+ */
+
 class MySingleton {
-    //
-    private static MySingleton INSTANCE;
-    private String info;
 
-    private MySingleton() {
-        info = "I'm a singleton instance";
-    }
+	private static MySingleton instanace;
 
-    // synchronized to make it thread safe in case of multi threded environment
-    public synchronized static MySingleton getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new MySingleton();
-        }
-        return INSTANCE;
-    }
+	private MySingleton() {
 
-    public String getInfo() {
-        return info;
-    }
+	}
 
-    public void setInfo(String info) {
-        this.info = info;
-    }
+	/*
+	 * Eager initialization -
+	 * 
+	 * Here we have created instance of singleton in static initializer. JVM
+	 * executes static initializer when the class is loaded and hence this is
+	 * guaranteed to be thread safe. Use this method only when your singleton class
+	 * is light and is used throughout the execution of your program.
+	 * 
+	 * class Singleton { private static Singleton obj = new Singleton();
+	 * 
+	 * private Singleton() {}
+	 * 
+	 * public static Singleton getInstance() { return obj; } }
+	 * 
+	 * 
+	 * 
+	 */
+
+	// Below three methods come under lazy initialization.
+
+	// Method one - not thread safe
+	public static MySingleton getInstance1() {
+
+		if (instanace == null) {
+			instanace = new MySingleton();
+		}
+		return instanace;
+
+	}
+
+	// Method 2 - Tread Safe but not efficient
+
+	public static synchronized MySingleton getInstance2() {
+
+		if (instanace == null) {
+			instanace = new MySingleton();
+		}
+
+		return instanace;
+	}
+
+	// Method 3 - Thread safe and efficient. Double checking locking
+
+	public static MySingleton getInstace3() {
+
+		if (instanace == null) {
+
+			synchronized (MySingleton.class) {
+
+				if (instanace == null) {
+
+					instanace = new MySingleton();
+				}
+			}
+
+		}
+
+		return instanace;
+
+	}
+
 }
