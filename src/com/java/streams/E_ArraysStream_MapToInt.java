edb33f0f4ab85211871d4ede77309b901845f2c7
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-08-02 21:11:57 +0530

    Update E_ArraysStream_MapToInt.java

--- src/com/java/streams/E_ArraysStream_MapToInt.java
+++ src/com/java/streams/E_ArraysStream_MapToInt.java
@@ -14,6 +14,16 @@ public class E_ArraysStream_MapToInt {
 		String str1 = "Hello man how are you";
 
 		/**
+		 * 
+		 * Usually List, Set, Map - we can easily create stream from them but what about
+		 * when we have arrays of primitive line int[], double[], float[] ????
+		 * 
+		 * 
+		 * Then how to create stream for primitive array : in that case we can use the
+		 * Arrays.stream() utility to create the stream and perform all the operation
+		 * needed.
+		 * 
+		 * 
 		 * This API will get the array of string and make the stream and then convert it
 		 * into intStream and convert that into array.
 		 * 
