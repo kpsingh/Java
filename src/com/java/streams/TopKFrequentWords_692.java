559cd3539eccc55e36207ff54839226107188574
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-19 22:18:25 +0530

    Update TopKFrequentWords_692.java

--- src/com/java/streams/TopKFrequentWords_692.java
+++ src/com/java/streams/TopKFrequentWords_692.java
@@ -1,8 +1,12 @@
 package com.java.streams;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
@@ -24,20 +28,39 @@ public class TopKFrequentWords_692 {
 
 	public static List<String> topKFrequent(String[] words, int k) {
 
+		Map<String, Integer> map = new HashMap<>(); // To make sure key's are sorted order
+
+		for (String w : words) {
+			map.merge(w, 1, (V1, V2) -> V1 + V2);
+		}
+
+		Queue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a,
+				b) -> (a.getValue() == b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue() - a.getValue()));
+
+		for (Map.Entry<String, Integer> e : map.entrySet()) {
+			pq.offer(e);
+		}
+		List<String> result = new ArrayList<>();
+		while (k > 0) {
+			k--;
+			result.add(pq.poll().getKey());
+		}
+		return result;
+
+	}
+
+	public static List<String> topKFrequent_OLD(String[] words, int k) {
+
 		Map<String, Integer> map = new TreeMap<>();
 
 		for (String s : words) {
 			map.merge(s, 1, (v1, v2) -> v1 + v2);
 
 		}
-		
+
 		/**
 		 * 
-		 * for(String w : words){
-            int c = map.getOrDefault(w,0);
-            c++;
-            map.put(w,c);
-        }
+		 * for(String w : words){ int c = map.getOrDefault(w,0); c++; map.put(w,c); }
 		 */
 
 		return map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(k)
