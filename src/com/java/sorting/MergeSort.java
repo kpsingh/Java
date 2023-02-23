b13abccc679ac5d14be4cfd10d16e129796ae7b9
Krishna Pratap Singh <kpsingh1234@gmail.com>
2023-02-23 16:10:31 +0530

    Create MergeSort.java

---
+++ src/com/java/sorting/MergeSort.java
@@ -0,0 +1,62 @@
+package com.java.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 6, 4, 2, 3, 7, 5 };
+		arr = sort(arr, 0, arr.length - 1);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	private static int[] sort(int[] arr, int low, int high) {
+
+		if (low == high) {
+
+			return new int[] { arr[low] };
+		}
+
+		int mid = low + (high - low) / 2;
+
+		int[] left = sort(arr, low, mid);
+
+		int[] right = sort(arr, mid + 1, high);
+
+		return merge(left, right);
+
+	}
+
+	private static int[] merge(int[] left, int[] right) {
+
+		int n = left.length;
+		int m = right.length;
+
+		int[] result = new int[n + m];
+
+		int i = 0, j = 0, k = 0;
+
+		while (i < n && j < m) {
+
+			if (left[i] < right[j]) {
+				result[k++] = left[i++];
+
+			} else {
+				result[k++] = right[j++];
+			}
+
+		}
+
+		while (i < n) {
+			result[k++] = left[i++];
+		}
+		while (j < m) {
+			result[k++] = right[j++];
+		}
+
+		return result;
+
+	}
+
+}
