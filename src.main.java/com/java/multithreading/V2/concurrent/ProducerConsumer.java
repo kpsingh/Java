342b78894135dfe5556699eb2d9f145a6d712cad
Krishna Pratap Singh <kpsingh1234@gmail.com>
2023-03-31 08:58:59 +0530

    Create ProducerConsumer.java

---
+++ src.main.java/com/java/multithreading/V2/concurrent/ProducerConsumer.java
@@ -0,0 +1,86 @@
+package com.java.multithreading.V2.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ProducerConsumer {
+
+	public static void main(String[] args) {
+
+		ExecutorService service = Executors.newFixedThreadPool(2);
+
+		Print p = new Print(1);
+
+		service.execute(() -> p.printEven());
+		service.execute(() -> p.printOdd());
+
+		service.shutdown();
+
+	}
+
+}
+
+class Print {
+
+	private int count;
+
+	Lock lock = new ReentrantLock();
+	Condition isEven = lock.newCondition();
+	Condition isOdd = lock.newCondition();
+
+	public Print(int count) {
+		this.count = count;
+	}
+
+	public void printEven() {
+
+		while (count < 10) {
+
+			try {
+				lock.lock();
+				
+				if (count % 2 != 0) {
+					isEven.await();
+				}
+				System.out.println("Even : " + count++);
+				isOdd.signal();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+			}
+
+		}
+
+		System.out.println("Finished producing Even Number");
+	}
+
+	public void printOdd() {
+
+		while (count < 10) {
+
+			try {
+				lock.lock();
+				
+				if (count % 2 == 0) {
+					isOdd.await();
+				}
+				
+				System.out.println("Odd : " + count++);
+				isEven.signal();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+			}
+
+		}
+
+		System.out.println("Finished producing Odd Number");
+	}
+}
