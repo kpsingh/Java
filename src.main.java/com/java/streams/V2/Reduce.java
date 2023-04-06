b0302c3e1bf14d82c31d2cc93f5063aed9fd43af
Krishna Pratap Singh <kpsingh1234@gmail.com>
2023-04-06 14:09:57 +0530

    Update Reduce.java

--- src.main.java/com/java/streams/V2/Reduce.java
+++ src.main.java/com/java/streams/V2/Reduce.java
@@ -19,10 +19,14 @@ public class Reduce {
 		/*
 		 * Simulations of parallel calculation.. lets assume we have two core(s)
 		 * 
+		 * This is how parallel stream works.. they divide the whole list/array into n
+		 * parts and calculate the results for each parts independent of each other and
+		 * the apply the same operation again on the result computed by each core
+		 * 
 		 */
 
-		List<Integer> list1 = Arrays.asList(0, 1, 2, 3, 4);
-		List<Integer> list2 = Arrays.asList(5, 6, 7, 8, 9);
+		List<Integer> list1 = Arrays.asList(0, 1, 2, 3, 4); // core 1 computation
+		List<Integer> list2 = Arrays.asList(5, 6, 7, 8, 9); // core 2 computation
 
 		int reduction1 = reduce(list1, 0, op);
 		int reduction2 = reduce(list2, 0, op);
@@ -30,7 +34,9 @@ public class Reduce {
 		System.out.println(reduction1);
 		System.out.println(reduction2);
 
-		reduction = reduce(Arrays.asList(reduction1, reduction2), 0, op);
+		reduction = reduce(Arrays.asList(reduction1, reduction2), 0, op); // merging of the computation from core 1 and
+																			// core 2
+
 		System.out.println(reduction);
 
 	}
