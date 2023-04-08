412aecd2b39b9cbe655287d41f33c4d51867384e
Krishna Pratap Singh <kpsingh1234@gmail.com>
2023-04-08 17:20:45 +0530

    Create MatchReduction.java

---
+++ src.main.java/com/java/streams/V2/MatchReduction.java
@@ -0,0 +1,13 @@
+package com.java.streams.V2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MatchReduction {
+	
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+		
+	}
+
+}
