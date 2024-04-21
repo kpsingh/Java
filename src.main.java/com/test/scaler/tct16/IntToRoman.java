d2ccf78e2bd433d42730b5d7e44b447bfbd05ff5
Krishna Pratap Singh <kpsingh1234@gmail.com>
2024-04-21 15:45:00 +0530

    TCT 1.6

--- src.main.java/com/test/scaler/tct16/IntToRoman.java
+++ src.main.java/com/test/scaler/tct16/IntToRoman.java
@@ -10,16 +10,18 @@ public class IntToRoman {
 
 
     public static void main(String[] args) {
+        int num = 1279;
+        System.out.println(intToRoman(num));
 
     }
 
-    public String intToRoman(int num) {
+    public static String intToRoman(int num) {
         StringBuilder sb = new StringBuilder();
         // Loop through each symbol, stopping if num becomes 0.
         for (int i = 0; i < values.length && num > 0; i++) {
             // Repeat while the current symbol still fits into num.
             while (values[i] <= num) {
-                num -= values[i];
+                num =  num - values[i];
                 sb.append(symbols[i]);
             }
         }
