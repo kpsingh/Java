9b6d475739fc99abffb5f3b8817b5f53535a6d31
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-02-05 22:33:26 +0530

    NumberOfIceland

--- src.main.java/com/java/graph/NumberOfIceland.java
+++ src.main.java/com/java/graph/NumberOfIceland.java
@@ -1,6 +1,14 @@
 package com.java.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NumberOfIceland {
+
+    private static final int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+    private static final int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
+
+
     public static void main(String[] args) {
         //int[][] arr = {{0, 1, 0}, {0, 0, 1}, {1, 0, 0}};
         int[][] arr = {{1, 1, 0, 0, 0}, {0, 1, 0, 0, 0}, {1, 0, 0, 1, 1}, {0, 0, 0, 0, 0}, {1, 0, 1, 0, 1}};
@@ -13,6 +21,7 @@ public class NumberOfIceland {
         int m = A[0].length;
         boolean[][] visited = new boolean[n][m];
         int count = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (A[i][j] == 1 && !visited[i][j]) {
@@ -24,23 +33,36 @@ public class NumberOfIceland {
         return count;
     }
 
-    private static void bfs(int[][] a, boolean[][] visited, int i, int j) {
-        if (i < 0 || j < 0 || i >= a.length || j >= a[0].length || visited[i][j] || a[i][j] == 0) {
-            return;
+    private static void bfs(int[][] arr, boolean[][] visited, int x, int y) {
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(x, y));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Pair cell = queue.poll();
+            int i = cell.i, j = cell.j;
+
+            for (int d = 0; d < 8; d++) {
+                int ni = i + dx[d], nj = j + dy[d];
+                if (isValid(arr, visited, ni, nj)) {
+                    queue.add(new Pair(ni, nj));
+                    visited[ni][nj] = true;
+                }
+            }
         }
+    }
 
-        visited[i][j] = true;
+    private static boolean isValid(int[][] arr, boolean[][] visited, int i, int j) {
+        return i >= 0 && j >= 0 && i < arr.length && j < arr[0].length && arr[i][j] == 1 && !visited[i][j];
+    }
+}
 
-        // now explore all the 8 side from the current (i, j) position.
-        bfs(a, visited, i - 1, j);
-        bfs(a, visited, i + 1, j);
-        bfs(a, visited, i, j - 1);
-        bfs(a, visited, i, j + 1);
-        bfs(a, visited, i - 1, j - 1);
-        bfs(a, visited, i + 1, j + 1);
-        bfs(a, visited, i - 1, j + 1);
-        bfs(a, visited, i + 1, j - 1);
+class Pair {
+    int i;
+    int j;
 
+    Pair(int i, int j) {
+        this.i = i;
 
     }
 }
