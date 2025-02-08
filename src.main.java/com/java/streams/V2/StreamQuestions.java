04ccfe448772677f57c814b1b3ede28155f1c735
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-02-08 17:53:14 +0530

    Calculate the average age of a list of Person objects using Java streams:

--- src.main.java/com/java/streams/V2/StreamQuestions.java
+++ src.main.java/com/java/streams/V2/StreamQuestions.java
@@ -11,7 +11,20 @@ public class StreamQuestions {
         List<String> names = Arrays.asList("KP", "Modi", "Ramayan", "Mahabharat");
         names.stream().sorted((s1, s2) -> s2.length() - s1.length()).limit(1).forEach(System.out::println);
         names.stream().max(Comparator.comparing(String::length)).ifPresent(System.out::println);
+        names.stream().min(Comparator.comparing(String::length)).ifPresent(System.out::println);
         names.stream().max((s1, s2) -> s1.length() - s2.length()).ifPresent(System.out::println);
+
+        // Calculate the average age of a list of Person objects using Java streams:
+        List<Person> persons = Arrays.asList(
+                new Person("Alice", 25),
+                new Person("Bob", 30),
+                new Person("Charlie", 35)
+        );
+        persons.stream().mapToInt(Person::getAge).average().orElse(0);
+        persons.stream().mapToInt(Person::getAge).average().ifPresent(System.out::println);
+
+
+
 
     }
 }
