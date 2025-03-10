314fca92515d49702afbf910fc791f8e8f24d3fc
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-03-10 18:44:02 +0530

    SubArrayWithGivenSum

--- src.main.java/com/java/towpointer/SubArrayWithGivenSum.java
+++ src.main.java/com/java/towpointer/SubArrayWithGivenSum.java
@@ -1,38 +1,44 @@
 package com.java.towpointer;
 
-import java.util.Arrays;
-
 /*
-Given an array of positive integers A and an integer B, find and return first continuous subarray which adds to B.
+Given an array of positive integers A and an integer B, find and return first continuous subarray which
+adds to B.
 
 If the answer does not exist return an array with a single integer "-1".
 
 First sub-array means the sub-array for which starting index in minimum.
-
-
  */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubArrayWithGivenSum {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int B = 5;
-        int[] ans = findSubArray(arr, B);
-        System.out.println(Arrays.toString(ans));
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        int target = 5;
+        List<Integer> result = solve(list, target);
+        System.out.println(result);
     }
 
-    private static int[] findSubArray(int[] arr, int b) {
-        int left = 0, right = 1;
-        int sum = arr[left];
-        while (right < arr.length) {
-            sum += arr[right];
-            if (sum == b) {
-                return new int[]{left, right};
-            }else if (sum < b) {
-                right++;
-            }else if (sum > b) {
-                sum -= arr[left];
-                left++;
+    private static List<Integer> solve(List<Integer> list, int target){
+        ArrayList<Integer> result = new ArrayList<>();
+        int low = 0;
+        int sum = 0;
+
+        for(int hi = 0; hi < list.size(); hi++){
+            sum += list.get(hi); // expand the window
+
+            while(sum > target && low <= hi){ // reduce the window if needed
+                sum -= list.get(low);
+                low++;
+            }
+            if(sum == target){
+                return new ArrayList<>(list.subList(low, hi + 1));
             }
         }
-        return new int[] {-1};
+
+        result.add(-1); // if no subarray found
+        return  result;
     }
 }
