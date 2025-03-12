37645d6a5f21b39bc0c5fbebab07b80b6721c96f
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-03-12 18:43:57 +0530

    Stream Practice

--- src.main.java/com/java/streams/V2/StreamAPIPractice.java
+++ src.main.java/com/java/streams/V2/StreamAPIPractice.java
@@ -1,13 +1,32 @@
 package com.java.streams.V2;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.OptionalDouble;
+import org.w3c.dom.ls.LSOutput;
+
+import java.awt.desktop.SystemEventListener;
+import java.util.*;
+import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamAPIPractice {
     public static void main(String[] args) {
 
+        List<Employee> employeeList = Employee.getEmployeeList();
+        /// Find out the highest experienced employees in the organization
+        employeeList.stream().min(Comparator.comparing(Employee::getYearOfJoining)).ifPresent(System.out::println);
 
+
+    }
+
+
+    private static boolean isPrime(int a) {
+        if (a <= 1) return false; // number less than 2 are not prime as they do not have the two dividers
+        for (int i = 2; i < Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
