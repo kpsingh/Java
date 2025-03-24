30c13cd88b84cccddb4d3260d2dd5425c9049069
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-03-24 21:34:24 +0530

    Evaluate Reverse Police Notation - Postfix

---
+++ src.main.java/com/java/stack/EvaluateExpression.java
@@ -0,0 +1,47 @@
+package com.java.stack;
+
+import java.util.*;
+
+public class EvaluateExpression {
+    public static void main(String[] args) {
+        //List<String> list = Arrays.asList("2", "1", "+", "3", "*");
+        List<String> list = Arrays.asList("2", "2", "/");
+        int ans = evalRPN(list);
+        System.out.println(ans);
+
+    }
+
+    private static int evalRPN(List<String> list) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String token : list) {
+            if (isOperator(token)) {
+                // perform the operation
+                int op2 = stack.pop(); // right hand side of
+                int op1 = stack.pop(); // left hand side of operand
+                int result = doOperation(token, op1, op2);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private static int doOperation(String token, int op1, int op2) {
+        switch (token) {
+            case "+" : return op1 + op2;
+            case "-" : return op1 - op2;
+            case "*" : return op1 * op2;
+            case "/" : return op1 / op2;
+            default : throw new IllegalArgumentException("Not valid operator");
+        }
+    }
+
+    private static boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-")
+                || token.equals("*") || token.equals("/");
+    }
+
+
+}
