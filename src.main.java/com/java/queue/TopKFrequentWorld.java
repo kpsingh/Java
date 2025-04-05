2c408e10525c908066ca928e2c6f1bedc82822be
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-04-05 19:30:55 +0530

    Find top K element

--- src.main.java/com/java/queue/TopKFrequentWorld.java
+++ src.main.java/com/java/queue/TopKFrequentWorld.java
@@ -12,9 +12,9 @@ Return the answer sorted by the frequency from highest to lowest. Sort the words
  */
 public class TopKFrequentWorld {
     public static void main(String[] args) {
-       // String[] arr = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
-        String[] arr = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
-        int k = 4;
+        //String[] arr = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
+        String[] arr = {"i", "love", "leetcode", "i", "love", "coding"};
+        int k = 3;
 
         List<String> ans = topKFrequent(arr, k);
         System.out.println(ans);
@@ -28,7 +28,15 @@ public class TopKFrequentWorld {
         }
 
         // create priority queue min priority queue based on the value of count
-        Queue<Map.Entry<String, Integer>> q = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        Queue<Map.Entry<String, Integer>> q = new PriorityQueue<>((e1, e2) -> {
+            if (Objects.equals(e1.getValue(), e2.getValue())) {
+                // If value are same then max heap so that the max key based on lexicographical order if value are same
+                return e2.getKey().compareTo(e1.getKey());
+            } else {
+                return e1.getValue() - e2.getValue(); // min heap based on the value
+            }
+        });
+
         for (Map.Entry<String, Integer> e : map.entrySet()) {
             q.add(e);
             if (q.size() > k) {
@@ -41,13 +49,11 @@ public class TopKFrequentWorld {
             list.add(q.poll());
         }
 
+        Collections.reverse(list);
+
         return list.stream()
-                .sorted((e1, e2) -> {
-                    if (e1.getValue() == e2.getValue()) {
-                        return e1.getKey().compareTo(e2.getKey());
-                    }
-                    return e2.getValue() - e1.getValue();
-                })
-                .map(Map.Entry::getKey).collect(Collectors.toList());
+                .map(Map.Entry::getKey)
+                //.sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 }
