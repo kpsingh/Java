0b62be0e9529bede59f76012fb3fa9a538398828
Krishna Pratap Singh <kpsingh1234@gmail.com>
2025-04-09 13:17:06 +0530

    NearestSmallerElement

---
+++ src.main.java/com/java/array/PartitionEqualSubsetSum.java
@@ -0,0 +1,28 @@
+package com.java.array;
+/*
+Partition Equal Subset Sum
+https://leetcode.com/problems/partition-equal-subset-sum/description/?envType=daily-question&envId=2025-04-07
+ */
+
+import java.util.stream.IntStream;
+
+public class PartitionEqualSubsetSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 11, 5};
+        boolean result = canPartition(arr);
+        System.out.println(result);
+    }
+
+    private static boolean canPartition(int[] arr) {
+        boolean result = false;
+
+        int sum = IntStream.of(arr).sum();
+        if(sum % 2 != 0) {
+            return result; //  sum is add then we can't divide in two parts
+        }
+
+
+
+        return result;
+    }
+}
---
+++ src.main.java/com/java/stack/NearestSmallerElement.java
@@ -0,0 +1,35 @@
+package com.java.stack;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+
+/*
+Input : A = [4, 5, 2, 10, 8]
+Output :  [-1, 4, -1, 2, 2]
+ */
+public class NearestSmallerElement {
+    public static void main(String[] args) {
+        //int[] arr = {4, 5, 2, 10, 8};
+        // int[] arr = {3,2,1};
+        int[] arr = {39,27,11,4,24,32,32,1};
+        ArrayList<Integer> result = prevSmaller(arr);
+        System.out.println(result);
+    }
+
+    private static ArrayList<Integer> prevSmaller(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int a : arr) {
+            while (!stack.isEmpty() && stack.peek() >= a) {
+                stack.pop();
+            }
+            result.add(stack.isEmpty() ? -1 : stack.peek());
+            stack.push(a);
+        }
+
+        return result;
+    }
+}
